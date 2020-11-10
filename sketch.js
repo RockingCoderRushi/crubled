78badfb2d7b48288d0dd6178d1cf156b703ea30d
RockingCoderRushi <72867633+RockingCoderRushi@users.noreply.github.com>
2020-11-10 16:26:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var downwall,rightwall,leftwall;
+//var downwall,rightwall,leftwall;
 var ground
 function preload()
 {
@@ -15,14 +15,14 @@ function setup() {
 	createCanvas(800, 700);
 	 
 	 ground = createSprite(600,680,1600,20)
-	downwall = createSprite(600,660,200,20)
-	downwall.shapeColor = "red"
+	//downwall = createSprite(600,660,200,20)
+	//downwall.shapeColor = "red"
 
-	rightwall = createSprite(690,600,20,100)
-	rightwall.shapeColor = "red"
+	//rightwall = createSprite(690,600,20,100)
+	//rightwall.shapeColor = "red"
 
-	leftwall = createSprite(510,600,20,100)
-	leftwall.shapeColor = "red"
+	//leftwall = createSprite(510,600,20,100)
+	//leftwall.shapeColor = "red"
 	engine = Engine.create();
 	world = engine.world;
 
